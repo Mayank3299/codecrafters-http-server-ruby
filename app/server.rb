229019d96d2e5cc08271a7f182b680ab65dc60c4
Mayank Agarwal <mayank.agarwal@awign.com@MacBook-Pro.local>
2025-05-27 16:57:15 +0530

    Handle 200 and 400

--- app/server.rb
+++ app/server.rb
@@ -1,4 +1,5 @@
 require "socket"
+require "byebug"
 
 # You can use print statements as follows for debugging, they'll be visible when running tests.
 print("Logs from your program will appear here!")
@@ -8,3 +9,11 @@ print("Logs from your program will appear here!")
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
 client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+
+request = client_socket.gets
+request = request.split(" ")
+if request[1] != '/'
+  client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+else
+  client.puts "HTTP/1.1 200 OK\r\n\r\n"
+end
