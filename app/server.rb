eef84669caa29e5466df9d6df4f13963a5591145
Mayank Agarwal <mayankagarwal@Mayanks-MacBook-Air.local>
2025-05-31 21:08:21 +0530

    code fix -2

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'socket'
-require 'byebug'
 
 # You can use print statements as follows for debugging, they'll be visible when running tests.
 print('Logs from your program will appear here!')
@@ -10,12 +9,11 @@ print('Logs from your program will appear here!')
 #
 server = TCPServer.new('localhost', 4221)
 client_socket, _client_address = server.accept
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
 
 request = client_socket.gets
 request = request.split(' ')
 if request[1] != '/'
-  client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+  client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
 else
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
+  client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
 end
