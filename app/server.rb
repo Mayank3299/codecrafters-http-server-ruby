d68f90dd87b8d3ced47c904f621ec3649307cc23
Mayank Agarwal <mayankagarwal@Mayanks-MacBook-Air.local>
2025-06-01 04:32:17 +0530

    Added user-agent endpoint

--- app/server.rb
+++ app/server.rb
@@ -11,13 +11,22 @@ print('Logs from your program will appear here!')
 server = TCPServer.new('localhost', 4221)
 client_socket, _client_address = server.accept
 
-request = client_socket.gets
-request = request.split(' ')
+request = []
+while (line = client_socket.gets)
+  break if line == "\r\n"
+
+  request << line.chomp
+end
+request.join.split("\r\n")
 # debugger
-if request[1].start_with?('/echo')
-  body = request[1][6..]
+request_line = request.first.split(' ')
+if request_line[1].start_with?('/echo')
+  body = request_line[1][6..]
   client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{body.length}\r\n\r\n#{body}"
-elsif request[1] == '/'
+elsif request_line[1].start_with?('/user-agent')
+  ua = request.find { |ele| ele.start_with?('User-Agent') }.split(': ').last
+  client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{ua.length}\r\n\r\n#{ua}"
+elsif request_line[1] == '/'
   client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
 else
   client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
